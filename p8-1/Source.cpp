cd835724bc502606766bcbdaf87080de5f847664
Elizabeth Burkholder <e-burkholder@onu.edu>
2023-10-19 09:19:34 -0400

    Completed countVowel function.

--- p8-1/Source.cpp
+++ p8-1/Source.cpp
@@ -2,16 +2,28 @@
 #include <string>
 using namespace std;
 
-int countCharacter(string str);
+int countVowel(string str);
 
 int main() {
 
-	cout<<countCharacter("hello");
-	cout<<countCharacter("1 2 3");
+	cout << countVowel("lkjlkjlkjlkjlkjo")<<endl;
+	cout << countVowel("1 2 3");
 
 }
 
-int countCharacter(string str) {
+int countVowel(string str) {
 	int characters = str.length();
-	return characters;
+	int vowels = 0;
+	for (int i = 0; i <= characters; i++) {
+		str.substr(i, 1);
+		if (str.substr(i, 1) == "A" || str.substr(i, 1) == "a" ||
+			str.substr(i, 1) == "E" || str.substr(i, 1) == "e" ||
+			str.substr(i, 1) == "I" || str.substr(i, 1) == "i" ||
+			str.substr(i, 1) == "O" || str.substr(i, 1) == "o" ||
+			str.substr(i, 1) == "U" || str.substr(i, 1) == "u")
+		{
+			vowels++;
+		}
+	}
+	return vowels;
 }
